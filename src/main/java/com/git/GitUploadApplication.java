d1b64a14b4907bb06e80233ea707216e66f7f2cb
Kabita <pradhankabita7873@gmail.com>
2023-07-11 17:26:55 +0530

    this is 2nd

---
+++ src/main/java/com/git/GitUploadApplication.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUploadApplication.class, args);
+	}
+
+}
