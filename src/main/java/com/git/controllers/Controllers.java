cf828b0d14cd0ee07a31d067028f3487b3b85a46
Kabita <pradhankabita7873@gmail.com>
2023-07-12 10:01:33 +0530

    profile sesstings done

--- src/main/java/com/git/controllers/Controllers.java
+++ src/main/java/com/git/controllers/Controllers.java
@@ -1,20 +1,28 @@
 package com.git.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.git.entity.Employee;
+import com.git.repositories.EmpRepository;
+
 @RestController
 @RequestMapping("/api")
 public class Controllers {
-	
+	@Autowired
+	private EmpRepository empRepo;
 	@GetMapping("/home")
 	public String home() {
 		return "Thsi is home page";
 	}
-	@GetMapping("/src")
-	public String SRC() {
-		return "Thsi is src page";
+	@GetMapping("/list")
+	public List<Employee> getAllEmpList() {
+		List<Employee> list = this.empRepo.findAll();
+		return list;
 	}
 	@GetMapping("/abc")
 	public String ABC() {
---
+++ src/main/java/com/git/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.git.entity;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private int phone;
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(Long id, String name, int phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+
+}
+
